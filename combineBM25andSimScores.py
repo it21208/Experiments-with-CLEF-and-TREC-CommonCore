c8f28fe015edec3ce611890bc2dc7173fe4f4e69
Alexandros T. Ioannidis ♐ <alexioanid@yahoo.gr>
2020-09-02 18:37:56 +0300

    Add files via upload

--- combineBM25andSimScores.py
+++ combineBM25andSimScores.py
@@ -1,4 +1,8 @@
 #  author: alexandros ioannidis
+
+# Does it really require you do that? Can you know think carefully about how documents are being scored, and why they would get a 
+# score or not? And alternatively, if a document is not scored by Lucene, can’t you just add that document to the end of the list with a score of zero?
+
 import sys 
 import operator
 
@@ -16,16 +20,15 @@ TOPIC_LIST_TEST_2018 = [
 
 TOPIC_LIST = TOPIC_LIST_TEST_2017 + TOPIC_LIST_TEST_2018
 # SimResults_filepath = '/home/pfb16181/simScoreResults2.txt'
-SimResults_filepath = '/home/pfb16181/results5.txt'
-BM25Results_filepath = '/home/pfb16181/RetrievalApp.pubmed5.seedDoc_title_and_query_and_queryExpansion_top20Terms_only_from_rel_docsResults.correct.res'
+# SimResults_filepath = '/home/pfb16181/results5.txt'
+SimResults_filepath = '/home/pfb16181/resultsFSSVmeanPoolingWithCorrectedCorpus.txt'
+BM25Results_filepath = '/home/pfb16181/RetrievalAppSubset.pubmed5.seedDoc_title_and_query_and_queryExpansion_top20Terms_only_from_rel_docsResults.correct.res'
 
 def readTRECresultsIntoDict(results_filepath):
     f = open(results_filepath, "r")
-    topic_doc_score_list = []
     topic_doc_score_dict = {}
     for i in TOPIC_LIST:
         topic_doc_score_dict[i] = {}
-    
     for line in f:
         topic = line.split()[0]
         doc = line.split()[2]
@@ -33,17 +36,15 @@ def readTRECresultsIntoDict(results_filepath):
         if topic not in TOPIC_LIST:
             continue
         else:
-            topic_doc_score_list.append([topic, [doc, score]])
-            #  dictionary with key the topic and value another dictionary with keys -> docs and values -> scores
             topic_doc_score_dict[topic][doc]= score
-    return(topic_doc_score_list, topic_doc_score_dict)
+    return(topic_doc_score_dict)
 
-BM25_topic_doc_score_list, BM25_topic_doc_score_dict = readTRECresultsIntoDict(BM25Results_filepath)
+BM25_topic_doc_score_dict = readTRECresultsIntoDict(BM25Results_filepath)
 print('finished creating BM25_topic_doc_score_list & BM25_topic_doc_score_dict')
-Sim_topic_doc_score_list, Sim_topic_doc_score_dict = readTRECresultsIntoDict(SimResults_filepath)
+Sim_topic_doc_score_dict = readTRECresultsIntoDict(SimResults_filepath)
 print('finished creating Sim_topic_doc_score_list & Sim_topic_doc_score_dict')
 
-LambdaParam_list = [0, 0.5, 1]
+LambdaParam_list = [0.0, 0.5, 1.0]
 lstToOutput = []
 
 for idx, ilambda in enumerate(LambdaParam_list):
@@ -52,34 +53,26 @@ for idx, ilambda in enumerate(LambdaParam_list):
         ranking = 1
         temp_dict = {}
         for keySim, cur_itemSim in Sim_topic_doc_score_dict[topic].items():
-            # for cur_itemSim in Sim_topic_doc_score_list:      
             for keyBM25, cur_itemBM25 in BM25_topic_doc_score_dict[topic].items():
-            # for cur_itemBM25 in BM25_topic_doc_score_list:
                 if keySim == keyBM25:
-                # if (cur_itemBM25[0] == topic) and (cur_itemSim[0] == topic) and (cur_itemBM25[1][0] == cur_itemSim[1][0]):
                     try:
-                        # v = ilambda * cur_itemBM25[1][1] + (1-ilambda) * cur_itemSim[1][1]
-                        v = ilambda * cur_itemBM25 + (1-ilambda) * cur_itemSim
-                        temp_dict[keySim] = v
-                                                
+                        temp_dict[keySim] = ilambda * float(cur_itemBM25) + (1-ilambda) * float(cur_itemSim)                      
                     except:
-                        v = cur_itemBM25
-                        temp_dict[keySim] = v
+                        print(float(cur_itemBM25))
+                        print(float(cur_itemSim))
+                    #     v = cur_itemBM25
+                    #     temp_dict[keySim] = v
                     
         sorted_d = dict(sorted(temp_dict.items(), key=operator.itemgetter(1),reverse=True))
-        print('sorted dictionary')
                       
         for k, v in sorted_d.items():
             temp = topic + " " + "QO" + " " + k + " " + str(ranking) + " "  + str(v) + " " + "similarity.tar.title.and.query"
             resultslst.append(temp)
             ranking += 1
                         
-        print('completed topic ', topic)
-    dirToWrite = '/home/pfb16181/NetBeansProjects/hedwig/scripts/resultsBM25andSim.%s' % ilambda
-    
+    dirToWrite = '/home/pfb16181/NetBeansProjects/hedwig/scripts/new_resultsBM25andSim.%s' % ilambda
     with open(dirToWrite, 'w') as f:
         for item in resultslst:
             f.write("%s\n" % item)
-
+            
     print('saved result file for lambda param = {}'.format(ilambda))
-    print('-----------')
