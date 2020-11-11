a7f3e43df29a7402155e7ca617ce64d9f4ac2609
Alexandros T. Ioannidis ♐  (°.°) <alexioanid@yahoo.gr>
2020-11-11 17:42:00 +0000

    Add files via upload

--- getSentsFor1718TESTCLEF.py
+++ getSentsFor1718TESTCLEF.py
@@ -7,17 +7,16 @@ import csv
 csv.field_size_limit(sys.maxsize)
 
 TOPICS_PATH = '/home/pfb16181/MeLIR/resources/topics/all.topics2017_2018_2019/'
-CLEF_TOPIC_LIST2017=[
+
+CLEF_TOPIC_LIST = [
     'CD007431', 'CD008081', 'CD008760', 'CD008782', 'CD008803', 'CD009135', 'CD009185', 'CD009372', 'CD009519', 'CD009551',
     'CD009579', 'CD009647', 'CD009786', 'CD009925', 'CD010023', 'CD010173', 'CD010276', 'CD010339', 'CD010386', 'CD010542',
-    'CD010633', 'CD010653', 'CD010705', 'CD010772', 'CD010775', 'CD010783', 'CD010860', 'CD010896', 'CD011145', 'CD012019'
-]
-CLEF_TOPIC_LIST2018 = [
+    'CD010633', 'CD010653', 'CD010705', 'CD010772', 'CD010775', 'CD010783', 'CD010860', 'CD010896', 'CD011145', 'CD012019',
     'CD008122', 'CD008587', 'CD008759', 'CD008892', 'CD009175', 'CD009263', 'CD009694', 'CD010213', 'CD010296', 'CD010502', 
     'CD010657', 'CD010680', 'CD010864', 'CD011053', 'CD011126', 'CD011420', 'CD011431', 'CD011515', 'CD011602', 'CD011686',
     'CD011912', 'CD011926', 'CD012009', 'CD012010', 'CD012083', 'CD012165', 'CD012179', 'CD012216', 'CD012281', 'CD012599'
 ]
-CLEF_TOPIC_LIST = CLEF_TOPIC_LIST2017 + CLEF_TOPIC_LIST2018
+
 LIST_OF_LISTs_OF_PMIDS_FOR_EACH_TOPIC = []
 for TOPIC in CLEF_TOPIC_LIST:
     TOPIC_FILE_PATH = TOPICS_PATH+TOPIC
@@ -51,8 +50,9 @@ with open('/home/pfb16181/NetBeansProjects/birch/data/datasets/clef171819_sents.
             docid = line.split('\t')[-3].split('_')[0]
             if docid in unique_flat_LIST_OF_LISTs_OF_PMIDS_FOR_EACH_TOPIC:
                 modified_line = row[0]
-                modified_line.split('\t')[-1] = cnt2
-                list_lines_saved_for_1718TestCLEF.append(modified_line.split('\t'))
+                temp = modified_line.split('\t') 
+                temp[-1] = str(cnt2)      
+                list_lines_saved_for_1718TestCLEF.append(temp)
                 cnt2 += 1
         except Exception:
             cnt += 1
